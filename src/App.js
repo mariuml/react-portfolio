b95debe75d9ddef4dcf925fa97275eb05e3502a7
mariuml <mariumladha97@gmail.com>
2023-03-06 11:05:18 +0000

    Created working homepage in Homepage.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "./components/Header"
+import Homepage from "./components/Homepage"
 
 function App() {
     return (
         <div>
             <Header />
+            <Homepage/>
         </div>
     )
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React from "react";
+import HeroImg from "../images/hero-img.png";
+
+// Styling for homepage
+
+const styles = {
+  heroImg: {
+    height: "300px",
+    width: "400px",
+    alignSelf: "center",
+    justifySelf: "center",
+    marginTop: "25px"
+   
+  },
+
+  bannerText: {
+    alignSelf: "center",
+    justifySelf: "center",
+    fontSize: "200%",
+    fontFamily: "'Open Sans', sans-serif",
+    color: "#7c7a7a"
+
+  },
+
+jumbotron: {
+    height: "400px",
+    backgroundColor: "#e9ecef",
+}
+};
+// Homepage function
 
 function Homepage() {
-    return (
-        <div>
-            
+  return (
+    <div className="jumbotron" style={styles.jumbotron} >
+      <div className="container">
+        <div className="row" >
+          <div className="col-sm" style={styles.bannerText}>
+            Developer and designer. Passionate about crafting sleek user
+            experiences.
+            <br></br>
+             Welcome to my portfolio.
+          </div>
+          <div className="col-sm">
+            <img src={HeroImg} style={styles.heroImg} />
+          </div>
         </div>
-    )
+      </div>
+
+    </div>
+    
+  );
 }
 
-export default Homepage
+export default Homepage;
