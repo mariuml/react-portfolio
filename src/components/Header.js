ee7182b935a4587f6f2cbc2398716ae334dd2ee9
mariuml <mariumladha97@gmail.com>
2023-03-13 11:51:07 +0000

    Responsive toggler working

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,29 +9,28 @@ const styles = {
 
 function Header() {
     return (
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <a className="navbar-brand" href="/">Marium Ladha // Web Developer </a>
 
-      <nav className="navbar navbar-expand-lg navbar-light bg-light" style={styles.navbar}>
-      <a className="navbar-brand" href="/" >Marium Ladha // Web Developer </a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse  " id="navbarNav" >
-        <ul className="navbar-nav ">
-    
-          <li className="nav-item">
-            <a className="nav-link" href="/"> Home </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="projects"> Projects </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="contact"> Contact me </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
-    )
+  <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="projects"> Projects </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="contact"> Contact Me </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+     )
 }
 
 export default Header
+
